45f4ed63a6f4e3a2064038efcb140d95a721b382
BryanVillabona <bvillabona13@gmail.com>
2025-03-30 18:43:57 -0500

    Update numeros.py

--- numeros.py
+++ numeros.py
@@ -1,4 +1,36 @@
-def encontrar_numero_primo(num):
-    for i in range(1,num+1):
-        if num % i == 0:
-            print(i)
+def encontrar_numeros_primos(limite):
+    primos = []
+    
+    for num in range(2, limite + 1):
+        rta = 0
+        for i in range(1, num + 1):
+            if num % i == 0:
+                rta += 1
+
+        if rta == 2:
+            primos.append(num)
+
+    return primos
+
+def encontrar_primos_gemelos(limite):
+    primos = encontrar_numeros_primos(limite)
+    gemelos = []
+
+    for i in range(len(primos) - 1):
+        if primos[i + 1] - primos[i] == 2:
+            gemelos.append((primos[i], primos[i + 1]))
+
+    return print(gemelos)
+
+def es_palindromo(n):
+    return str(n) == str(n)[::-1]
+
+def encontrar_primos_palindromicos(limite):
+    primos = encontrar_numeros_primos(limite) 
+    palindromicos = []  
+    
+    for num in primos:
+        if es_palindromo(num): 
+            palindromicos.append(num) 
+    
+    return print(palindromicos) 
